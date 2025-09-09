4a241955518cd9ae68bd9859e83958cb01447138
limrunbot <contact@limrun.com>
2025-09-10 01:16:02 +0300

    chore(examples): update to the latest @limrun/api
    Signed-off-by: limrunbot <contact@limrun.com>

--- examples/tunnel/index.ts
+++ examples/tunnel/index.ts
@@ -1,5 +1,5 @@
 import { Limrun } from '@limrun/api';
-import { startAdbTunnel } from '@limrun/api';
+import { createInstanceClient } from '@limrun/api';
 
 const apiKey = process.env['LIM_TOKEN'];
 
@@ -10,10 +10,20 @@ if (!apiKey) {
 
 const limrun = new Limrun({ apiKey });
 
-const androidInstance = await limrun.androidInstances.create({});
+// Wait makes sure the request returns only after the URLs are set and
+// the instance is ready to connect.
+console.time('create');
+const androidInstance = await limrun.androidInstances.create({ wait: true });
+console.log(`Instance ${androidInstance.metadata.id} created`);
+console.timeEnd('create');
 
-const { address, close } = await startAdbTunnel(androidInstance);
+const client = await createInstanceClient({
+  adbUrl: androidInstance.status.adbWebSocketUrl!,
+  endpointUrl: androidInstance.status.endpointWebSocketUrl!,
+  token: androidInstance.status.token,
+});
 
+const { address, close } = await client.startAdbTunnel();
 console.log(`ADB connected on ${address.address}:${address.port}`);
 console.log('You can run `adb devices` to see the connected device');
 console.log('Press Ctrl+C to exit or it will automatically close in 30 seconds');
